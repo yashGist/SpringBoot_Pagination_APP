cf21513215d0295c634a75824284110d16be8474
Yashdeep <ramawatyashdeep@gmail.com>
2025-09-25 04:54:54 +0530

    Pagination

---
+++ src/main/java/com/springbootpaginationapp/SpringBootPaginationAppApplication.java
@@ -0,0 +1,14 @@
+package com.springbootpaginationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPaginationAppApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootPaginationAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springbootpaginationapp/SpringBootPaginationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootpaginationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPaginationAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
